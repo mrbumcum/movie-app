e83f2fc1c95ccecfec18845b90104d7790c7148a
shucksow <102425362+shucksow@users.noreply.github.com>
2025-03-08 14:59:03 -0500

    feat: connect API

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,23 @@ const Navbar = () => {
           <Link to="/" className="hover:text-orange-500">Home</Link>
           <Link to="/movies" className="hover:text-orange-500">Movies</Link>
           <Link to="/tvshows" className="hover:text-orange-500">TV Shows</Link>
-          <Link to="/search" >Search</Link>
+          <Link to="/search" className="flex items-center hover:text-orange-500">
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              viewBox="0 0 24 24" 
+              fill="none" 
+              stroke="currentColor" 
+              strokeLinecap="round" 
+              strokeLinejoin="round" 
+              width="24" 
+              height="24" 
+              strokeWidth="2"
+              className="w-5 h-5"
+            >
+              <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+              <path d="M21 21l-6 -6"></path>
+            </svg>
+          </Link>
         </div>
       </div>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useEffect } from 'react'
+import { fetchTrending } from '../services/api'
 
 const Home = () => {
+  
+  useEffect(() => {
+    fetchTrending('day')
+      .then((res) => {
+        console.log(res, "res");
+      })
+      .catch((err) => {
+        console.log(err, "err");
+      })
+  }, [])
+  
+  
   return (
     <div>Home</div>
   )
